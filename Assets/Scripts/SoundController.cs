b8f9eea18780a1e0db7462d423298e2cd7ec5a59
Marcus Gåhlin <marcus.gahlin94@gmail.com>
2023-12-19 12:01:27 +0100

    Initial commit

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool isMuted = false;
+
+    public float scaleMultiplier = 1.0f;
+
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        originalScale = transform.localScale;
+
+    }
+
+    void Update()
+    {
+        if (!isMuted)
+        {
+            audioData();
+            scaleUpdate();
+        }
+        else
+        {
+            transform.localScale = originalScale;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+            {
+                Mute();
+            }
+        }
+    }
+
+    public void Mute()
+    {
+        isMuted = !isMuted;
+        audioSource.mute = isMuted;
+    }
+
+    void audioData()
+    {
+        float[] spectrumData = new float[256];
+
+        audioSource.GetSpectrumData(spectrumData, 0, FFTWindow.Hamming);
+
+        float audioLevel = 0f;
+
+        for (int i = 0; i < spectrumData.Length; i++)
+        {
+            audioLevel += spectrumData[i];
+        }
+        audioLevel /= spectrumData.Length;
+
+        scaleMultiplier = 1.0f + audioLevel * 200f;
+    }
+
+    void scaleUpdate()
+    {
+        transform.localScale = originalScale * scaleMultiplier;
+    }
+}
+
+
+
